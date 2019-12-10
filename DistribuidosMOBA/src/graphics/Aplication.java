4d7b4b041b174a173b3af66c7627323cedc4dada
daniel <daniel@lenovo>
2019-12-10 09:55:42 +0100

    save

--- DistribuidosMOBA/src/graphics/Aplication.java
+++ DistribuidosMOBA/src/graphics/Aplication.java
@@ -6,21 +6,26 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-public class Aplication extends Canvas{
+public class Aplication extends Canvas implements Runnable{
 
 	private static final long serialVersionUID = 1L;
 	
 	private static final int WIDTH = 800;
 	private static final int HEIGTH = 800;
 	
+	public static volatile boolean working = false;
+	
 	private static final String NAME = "M.O.B.A. UR-Distribuidos";
 	
 	private static JFrame window;
-
+	private static Thread thread;
+	
+	
 	public Aplication() {
 		setPreferredSize(new Dimension(WIDTH,HEIGTH));
 		
 		window = new JFrame(NAME);
+		
 		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
@@ -29,5 +34,22 @@ public class Aplication extends Canvas{
 		window.pack();
 		window.setLocationRelativeTo(null); //Centrado en el escritorio.
 		window.setVisible(true);
+	}
+
+	@Override
+	public void run() {
+		while(working) {
+			
+		}
+	}
+	
+	public synchronized void ini_thread() {
+		working = true;
+		thread = new Thread(this,"Graphics");
+		thread.start();
+	}
+	
+	public synchronized void stop_thread() {
+		working = false;
 	}
 }
